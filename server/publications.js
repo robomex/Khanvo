5646cdff30c327a5995dc7cabcfa76ee279d369b
robomex <atmorales@gmail.com>
2014-05-28 23:13:13 -0500

    dead simple logged-in-user subscription list

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,5 @@
 Meteor.publish('khanvos', function() {
-	return Khanvos.find();
+	return Khanvos.find({followers: this.userId});
 });
 Meteor.publish('posts', function(khanvoName) {
 	return Posts.find({khanvoName: khanvoName});
