27daca58e410896e85a4f1eaa52ee0d1c55a57ba
robomex <atmorales@gmail.com>
2014-06-16 00:20:11 -0500

    publish all usernames to client

--- server/publications.js
+++ server/publications.js
@@ -4,3 +4,6 @@ Meteor.publish('khanvos', function() {
 Meteor.publish('posts', function(khanvoName) {
 	return Posts.find({khanvoName: khanvoName});
 });
+Meteor.publish('allUsernames', function() {
+	return Meteor.users.find();
+});
