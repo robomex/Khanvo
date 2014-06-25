4ecc13b59b517a60fc96b4eda931ba73fec4383e
robomex <atmorales@gmail.com>
2014-06-24 21:44:05 -0500

    only subscribe to latest X posts

--- server/publications.js
+++ server/publications.js
@@ -5,8 +5,11 @@ Meteor.publish('khanvos', function(user) {
 		return Khanvos.find({followers: user.username});
 	}
 });
-Meteor.publish('posts', function(khanvoName) {
-	return Posts.find({khanvoName: khanvoName});
+Meteor.publish('posts', function(khanvoName, readStart) {
+	khanvo = Khanvos.findOne({khanvoName: khanvoName});
+	var limit = parseInt(khanvo.postCount - 10);
+	console.log(limit);
+	return Posts.find({$and: [ {khanvoName: khanvoName}, {postNumber: {$gt: limit}}]});
 });
 Meteor.publish('allUsernames', function() {
 	return Meteor.users.find();
